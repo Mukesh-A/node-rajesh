a28a9a0825761a2a057b5176df19b6bcc25b172f
Mukesh A <90620152+Mukesh-A@users.noreply.github.com>
2024-02-15 10:49:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const url =
-  "mongodb+srv://mukeshproject06:7p5MgszvgOmqHg8d@cluster0.umkbthn.mongodb.net/Mukesh?retryWrites=true&w=majority";
+  "";
 // mongoose.set("strictQuery", false);
 
 mongoose.connect(url, {
